974644eb7c08d19707f6bb5cc752829ee9d09695
HalimaJasmine <hdiallo@jasmineconseil.com>
2025-10-15 15:58:48 +0000

    update project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,15 +11,15 @@ const projects = [
     image: Frame,
     tags: ['React Native', 'Figma', 'UX-UI Design'],
     github: '#',
-    demo: '#'
+    demo: 'https://www.figma.com/proto/iNiUuFYgiqQY1xqtQ0qgaA/Untitled?node-id=52-2770&t=fj5UaUd4V6Xu1btA-1&scaling=scale-down&content-scaling=fixed&page-id=52%3A1878&starting-point-node-id=52%3A2770'
     },
     {
-    title: 'Dashboard Analytics',
-    description: 'Tableau de bord interactif avec visualisations de données et interface intuitive.',
+    title: 'Gestionnaire de tâches — Application web intuitive',
+    description: 'Elle permet d’ajouter, modifier, supprimer et organiser ses tâches par catégories, avec un système de filtrage et une interface simple et responsive.',
     image: Task,
     tags: ['Angular', 'D3.js', 'UX-UI Design'],
     github: '#',
-    demo: 'https://www.figma.com/proto/WSgWwR4vDU5FIbjzYQIWj1/Untitled?node-id=11-1898&p=f&t=QO1H9wMYBgOUv4G4-1&scaling=scale-down-width&content-scaling=fixed&page-id=11%3A1897'
+    demo: 'https://www.figma.com/proto/WSgWwR4vDU5FIbjzYQIWj1/Untitled?1898&p=f&t=QO1H9wMYBgOUv4G4-1&scaling=scale-down-width&content-scaling=fixed&page-id=11%3A1897'
     },
     {
     title: 'Site Web Corporate',
@@ -30,7 +30,6 @@ const projects = [
     demo: '#'
     }
     ];
-
 
 
 const Projects = () => {
@@ -79,14 +78,40 @@ const Projects = () => {
         >
         GitHub
         </Button>
-        <Button
+      {/* Demo */}
+      {project.demo && project.demo !== "#" ? (
+                    <a
+                      href={project.demo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label={`Voir la démo de ${project.title}`}
+                    >
+                      <Button
+                        type="primary"
+                        size="small"
+                        className="!rounded-button whitespace-nowrap cursor-pointer bg-pink-500 hover:bg-pink-600 border-pink-500"
+                      >
+                        Voir Demo
+                      </Button>
+                    </a>
+                  ) : (
+                    <Button
+                      type="primary"
+                      size="small"
+                      disabled
+                      className="!rounded-button whitespace-nowrap bg-gray-300 border-gray-300"
+                    >
+                      Demo Indisponible
+                    </Button>
+                  )}
+        {/* <Button
   type="primary"
   size="small"
   className="!rounded-button whitespace-nowrap cursor-pointer bg-pink-500 hover:bg-pink-600 border-pink-500"
   onClick={() => window.open('https://www.figma.com/proto/iNiUuFYgiqQY1xqtQ0qgaA/Untitled?node-id=52-2770&p=f&t=rFUiBNaN6GukZExp-1&scaling=scale-down&content-scaling=fixed&page-id=52%3A1878&starting-point-node-id=52%3A2770', '_blank')}
 >
   Voir Demo
-</Button>
+</Button> */}
 
         </div>
         </div>
